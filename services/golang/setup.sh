46af8b9e55408a4016ab2e39ba0fcaa1fc181955
Chad Metcalf <metcalfc@gmail.com>
2019-09-10 16:43:57 -0700

    Fixing the sed syntax for busybox sed.

--- services/golang/setup.sh
+++ services/golang/setup.sh
@@ -3,7 +3,7 @@
 cat /run/configuration
 
 # parse parameters
-parameters=$(jq -c '.services | map(select(.serviceId == "github-repo"))[0].parameters' /run/configuration)
+parameters=$(jq -c '.services | map(select(.serviceId == "golang-template"))[0].parameters' /run/configuration)
 username=$(echo "${parameters}" | jq -c '.username' --raw-output)
 docker_username=$(echo "${parameters}" | jq -c '.docker_username' --raw-output)
 accessToken=$(echo "${parameters}" | jq -c '.accessToken' --raw-output)
@@ -15,11 +15,11 @@ curl -XPOST -u ${username}:${accessToken} https://api.github.com/user/repos -d '
 echo "* You need to login to Docker Hub and create the repo."
 echo "* You need to login to GitHub and create the secretes for DOCKER_USERNAME/DOCKER_PASSWORD."
 
-find /project \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i '' "s/{{PROJECT}}/${repoName}/g"
-find /project \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i '' "s/{{GITHUB_ORG}}/${username}/g"
-find /project \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i '' "s/{{HUB_USER}}/${docker_username}/g"
-
 cp -r /files/* /project
+
+find /project \( -type d -name .git -prune \) -o -type f | xargs sed -i "s/{{PROJECT}}/${repoName}/g"
+find /project \( -type d -name .git -prune \) -o -type f | xargs sed -i "s/{{GITHUB_ORG}}/${username}/g"
+find /project \( -type d -name .git -prune \) -o -type f | xargs sed -i "s/{{HUB_USER}}/${docker_username}/g"
 
 cd /project
 
